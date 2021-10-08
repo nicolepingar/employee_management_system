82e065c4c70e8033b78daf741dd6b44946e1658b
Nicole Pingar <nicolepingar@yahoo.com>
2021-10-08 15:51:09 -0400

    env work

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 // required packages for the application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const mysql = require('mysql2');
+const cTable = require('console.table');
+require('dotenv').config()
